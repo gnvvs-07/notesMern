a257c378442f35392d3fc00ab203f7719da6467e
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-07-23 15:42:50 +0530

    data base connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,28 @@
-import express from "express"
-
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
-const PORT = 3000
-app.listen(PORT,()=>{console.log("server running")})
+// middle wares
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("DB connected");
+});
+// test api
+app.get("/api/", (req, res) => {
+  res.json({
+    message: "Hello World",
+    success: true,
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("server running");
+});
