eb61fb6364ef74979a309238a611f61eb3d9637d
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-07-25 12:24:21 +0530

    updating note api complete

--- api/controllers/notes.controller.js
+++ api/controllers/notes.controller.js
@@ -12,11 +12,36 @@ export const createNote = async (req, res, next) => {
       title,
       content,
       tags,
-      user:req.user.id,
+      user: req.user.id,
     });
 
     await newNote.save();
     res.status(201).json(newNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const edit = async (req, res, next) => {
+  try {
+    const { notesId, userId } = req.params;
+
+    // Check if the user is authorized to edit the note
+    if (req.user.id !== userId) {
+      return next(new Error("User not authorized to edit this note"));
+    }
+
+    const updatedNote = await Notes.findByIdAndUpdate(
+      notesId,
+      { $set: req.body },
+      { new: true }
+    );
+
+    if (!updatedNote) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    res.status(200).json(updatedNote);
   } catch (error) {
     next(error);
   }
--- api/routes/notes.route.js
+++ api/routes/notes.route.js
@@ -1,6 +1,7 @@
 import express from "express"
-import { createNote } from "../controllers/notes.controller.js";
+import { createNote, edit } from "../controllers/notes.controller.js";
 import {verifyToken} from "../tokens/verifyToken.js";
 const router = express.Router()
 router.post("/create",verifyToken,createNote);
+router.put("/edit/:notesId/:userId",verifyToken,edit);
 export default router;
